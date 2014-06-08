77f988698f8a48076c22af95995c4ff1582c7aea
Dave Rael <dave@raelyard.com>
2014-06-08 07:24:04 -0500

    got implementation complete for tracking a sickness episode event with an explicit start date/time

---
+++ BadassTracker.Model.Tests/Events/DomainServices/SicknessEpisodeEventManagerTests/WhenCreatingASicknessEpisodeEventWithStartDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BadassTracker.Model.Events.Creation;
+
+namespace BadassTracker.Model.Tests.Events.DomainServices.SicknessEpisodeEventManagerTests
+{
+    public class WhenCreatingASicknessEpisodeEventWithStartDate : WhenCreatingASicknessEpisodeEventBase
+    {
+        private const string StartDateTimeString = "2040-01-01";
+        private readonly DateTime _startDateTime = DateTime.Parse(StartDateTimeString);
+
+        protected override Action<SicknessEpisodeEventBuilder> BuilderAction
+        {
+            get { return builder => builder.StartingAt(_startDateTime); }
+        }
+    }
+}
